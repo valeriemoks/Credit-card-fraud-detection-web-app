e566689854644ce765ed3415731ad5d5a2daa262
valeriemoks <126461341+valeriemoks@users.noreply.github.com>
2023-06-24 06:20:22 +0300

    app.py

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+from model import model
 app = Flask(__name__)
 
 # Load the model
 model = pickle.load(open("model.pkl", "rb"))
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the form data
     distance_from_home = float(request.form['distance_from_home'])
-    distance_from_last_transaction = float(request.form['distance_from_last_transaction'])
-    ratio_to_median_purchase_price = float(request.form['ratio_to_median_purchase_price'])
+    distance_from_last_transaction = float(
+        request.form['distance_from_last_transaction'])
+    ratio_to_median_purchase_price = float(
+        request.form['ratio_to_median_purchase_price'])
     repeat_retailer = int(request.form['repeat_retailer'])
     used_chip = int(request.form['used_chip'])
     used_pin_number = int(request.form['used_pin_number'])
@@ -40,6 +44,7 @@ def predict():
         return render_template('index.html', prediction_text='The prediction is: Fraudulent Transaction')
     else:
         return render_template('index.html', prediction_text='The prediction is: Legitimate Transaction')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
