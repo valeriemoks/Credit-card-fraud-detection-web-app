c76e060f78f0d1421a8bf3c9f2d7622fc378efd6
valeriemoks <126461341+valeriemoks@users.noreply.github.com>
2023-06-24 19:33:21 +0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from model import model
+
 app = Flask(__name__)
 
 # Load the model
